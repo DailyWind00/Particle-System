20a9472d87aaff52b2136019473d030c2d0caef5
DailyWind <140258565+DailyWind00@users.noreply.github.com>
2024-11-29 14:46:54 +0100

    Update program_loop

--- srcs/routine.cpp
+++ srcs/routine.cpp
@@ -2,15 +2,21 @@
 
 // Keep the window alive, exiting this function mean the process is over
 static void program_loop(GLFWwindow *window, Shader &shaders, ParticleSystem &particles) {
+	double time = 0.0;
+
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		getFrametime();
 		handleEvents(window, shaders);
 
-		particles.update();
+		particles.update(time);
 		particles.draw();
 
+		shaders.setFloat("time", time);
+
 		glfwSwapBuffers(window);
+
+		time += 0.01;
 	}
 }
 
@@ -20,6 +26,8 @@ void	Rendering(GLFWwindow *window, size_t particleCount) {
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	// glEnable(GL_PROGRAM_POINT_SIZE);
+	// glPointSize(500.0f);
 
 	vector<string> VkernelProgramPaths = {
 		"./srcs/kernels/test.cl"
